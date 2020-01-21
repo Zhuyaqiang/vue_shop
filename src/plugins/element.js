03792e88ca31d5257e0f1c23cdab2b4039a3a40c
Zhuyaqiang <zyq1512099831@gmail.com>
2020-01-21 10:21:13 +0800

    完成用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,33 @@
 import Vue from 'vue'
 // eslint-disable-next-line no-unused-vars,import/no-duplicates
-import { Button, Form, FormItem, Input } from 'element-ui' // 按需导入
-// eslint-disable-next-line import/no-duplicates
-import { Message } from 'element-ui'
+import {
+  Button, Form, FormItem, Input, Message, Container,
+  Header, Aside, Main, Menu, Submenu, MenuItem,
+  Breadcrumb, BreadcrumbItem, Card, Row, Col, Table,
+  TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox
+} from 'element-ui' // 按需导入
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 // 全局挂载
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
